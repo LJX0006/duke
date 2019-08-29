554e55f27da4e0a0e4ad57b5788b5643aa514c48
LJX0006 <liujxcindy@gmail.com>
2019-08-29 09:17:19 +0800

    level-7

---
+++ src/main/java/Save.java
@@ -0,0 +1,79 @@
+import java.io.*;
+
+public class Save {
+    private static Task[] item = new Task[100];
+    private static int n = 0;
+
+    public Save() {
+        try {
+            FileReader fileReader = new FileReader("data/duke.txt");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String str;
+            while ((str = bufferedReader.readLine()) != null) {
+                str = str.replace('|', '/');
+                String[] detail = str.split(" / ");
+                Task t = new Task("waiting");
+                if (detail[0].equals("T")) {
+                    t = new Todo(detail[2]);
+                }
+                else if (detail[0].equals("D")) {
+                    t = new Deadline(detail[2], detail[3]);
+                }
+                else {
+                    t = new Event(detail[2], detail[3]);
+                }
+                if (detail[1].equals("1")) {
+                    t.markAsDone();
+                }
+                item[n++] = t;
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Task[] getDetail() {
+        return item;
+    }
+
+    public int getNum() {
+        return n;
+    }
+
+    public void writeFile(Task[] List) {
+        try {
+            FileWriter fileWriter = new FileWriter("data/duke.txt");
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write("");
+            for (Task t : List) {
+                if (t instanceof Todo) {
+                    if (t.isDone)
+                        bufferedWriter.write(t.symbol + " | 1 | " + t.description + "\n");
+                    else
+                        bufferedWriter.write(t.symbol + " | 0 | " + t.description + "\n");
+                }
+                else if (t instanceof Deadline) {
+                    if (t.isDone)
+                        bufferedWriter.write(t.symbol + " | 1 | " + t.description + " | "
+                                + ((Deadline) t).by + "\n");
+                    else
+                        bufferedWriter.write(t.symbol + " | 0 | " + t.description + " | "
+                                + ((Deadline) t).by + "\n");
+
+                }
+                else if (t instanceof Event) {
+                    if (t.isDone)
+                        bufferedWriter.write(t.symbol + " | 1 | " + t.description + " | "
+                                + ((Event) t).at + "\n");
+                    else
+                        bufferedWriter.write(t.symbol + " | 0 | " + t.description + " | "
+                                + ((Event) t).at + "\n");
+                }
+            }
+            bufferedWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,6 +1,7 @@
 public class Todo extends Task {
     public Todo(String description) {
         super(description);
+        super.symbol = "T";
     }
     @Override
     public String toString() {
